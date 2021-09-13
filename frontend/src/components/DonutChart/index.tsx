59a46ec76c1717d64279bd3fee4fc00d8de3976e
Danilo Pereira <danilo.pereira@desenvolvecidade.com>
2021-09-13 00:34:21 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -25,11 +25,6 @@ const DonutChart = () => {
     });
   }, []);
 
-  const mockData = {
-    series: [477138, 499928, 444867, 220426, 473088],
-    labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-  };
-
   const options = {
     legend: {
       show: true,
@@ -39,7 +34,7 @@ const DonutChart = () => {
   return (
     <Chart
       options={{ ...options, labels: chartData.labels }}
-      series={mockData.series}
+      series={chartData.series}
       type="donut"
       height="240"
     />
